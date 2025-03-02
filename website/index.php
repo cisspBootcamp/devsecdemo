e86f43f3e5e65511aef22191d87afd28803a9012
cisspBootcamp <150753410+cisspBootcamp@users.noreply.github.com>
2025-03-02 13:20:24 -0500

    Update index.php

--- website/index.php
+++ website/index.php
@@ -5,7 +5,7 @@ $bdd = mysqli_connect("localhost","demo","mypassword","demobd") or die("Not conn
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>CISSP BootCamp - v0.3<title>
+<title>CISSP BootCamp - v0.4<title>
 <style type="text/css">
 div#container
 {
